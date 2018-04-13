790983306b4d064e4fab1d04e815411c9f0b4e30
Amsekal <33755443+Amsekal@users.noreply.github.com>
2018-04-13 14:19:58 +0300

    Update szerverre_feltoltes.py

--- szerverre_feltoltes.py
+++ szerverre_feltoltes.py
@@ -2,4 +2,5 @@ import sqlite3
 conn = sqlite3.connect('ertekek.db')
 c = conn.cursor()
 c.execute("INSERT INTO erzekeklok VALUES (1,strftime('%Y-%m-%d %H:%M:%S'),3,4,3,4)")
+conn.commit()
 conn.close()
